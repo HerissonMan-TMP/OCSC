7a41e4bee88f65a5fc772144fd9124415604cd13
HerissonMan-TMP <013scrat@gmail.com>
2021-03-05 15:38:33 +0100

    Added classes to fix a bug with responsivity

--- resources/views/staff/hub.blade.php
+++ resources/views/staff/hub.blade.php
@@ -9,7 +9,7 @@ Staff Hub
 @section('content-staff')
 <div class="mt-6 px-4 py-5 sm:p-6 bg-gray-800 rounded-md shadow overflow-hidden grid grid-cols-6 gap-4">
     <a href="{{ route('staff.news-management') }}" class="col-span-full md:col-span-3">
-        <div class="p-8 text-gray-300 bg-gray-900 hover:bg-gray rounded-md text-center font-light tracking-wide">
+        <div class="w-full h-full p-8 text-gray-300 bg-gray-900 hover:bg-gray rounded-md text-center font-light tracking-wide">
             <div class="mb-5">
                 <i class="flex-shrink-0 text-gray-300 fas fa-newspaper fa-fw fa-5x"></i>
             </div>
@@ -17,7 +17,7 @@ Staff Hub
         </div>
     </a>
     <a href="{{ route('staff.convoy-management') }}" class="col-span-full md:col-span-3">
-        <div class="p-8 text-gray-300 bg-gray-900 hover:bg-gray rounded-md text-center font-light tracking-wide">
+        <div class="w-full h-full p-8 text-gray-300 bg-gray-900 hover:bg-gray rounded-md text-center font-light tracking-wide">
             <div class="mb-5">
                 <i class="flex-shrink-0 text-gray-300 fas fa-truck-moving fa-fw fa-5x"></i>
             </div>
@@ -25,7 +25,7 @@ Staff Hub
         </div>
     </a>
     <a href="{{ route('staff.partnership-management') }}" class="col-span-full md:col-span-2">
-        <div class="p-8 text-gray-300 bg-gray-900 hover:bg-gray rounded-md text-center font-light tracking-wide">
+        <div class="w-full h-full p-8 text-gray-300 bg-gray-900 hover:bg-gray rounded-md text-center font-light tracking-wide">
             <div class="mb-5">
                 <i class="flex-shrink-0 text-gray-300 fas fa-handshake fa-fw fa-5x"></i>
             </div>
@@ -33,7 +33,7 @@ Staff Hub
         </div>
     </a>
     <a href="{{ route('staff.gallery-management') }}" class="col-span-full md:col-span-2">
-        <div class="p-8 text-gray-300 bg-gray-900 hover:bg-gray rounded-md text-center font-light tracking-wide">
+        <div class="w-full h-full p-8 text-gray-300 bg-gray-900 hover:bg-gray rounded-md text-center font-light tracking-wide">
             <div class="mb-5">
                 <i class="flex-shrink-0 text-gray-300 fas fa-images fa-fw fa-5x"></i>
             </div>
@@ -41,7 +41,7 @@ Staff Hub
         </div>
     </a>
     <a href="{{ route('staff.contact-messages.index') }}" class="col-span-full md:col-span-2">
-        <div class="p-8 text-gray-300 bg-gray-900 hover:bg-gray rounded-md text-center font-light tracking-wide">
+        <div class="w-full h-full p-8 text-gray-300 bg-gray-900 hover:bg-gray rounded-md text-center font-light tracking-wide">
             <div class="mb-5">
                 <i class="flex-shrink-0 text-gray-300 fas fa-envelope fa-fw fa-5x"></i>
             </div>
@@ -49,7 +49,7 @@ Staff Hub
         </div>
     </a>
     <a @can('has-admin-rights') href="{{ route('staff.role-permission-management') }}" @endcan class="col-span-full md:col-span-2 @cannot('has-admin-rights') opacity-30 @endcannot">
-        <div class="p-8 text-gray-300 bg-gray-900 hover:bg-gray rounded-md text-center font-light tracking-wide">
+        <div class="w-full h-full p-8 text-gray-300 bg-gray-900 hover:bg-gray rounded-md text-center font-light tracking-wide">
             <div class="mb-5">
                 <i class="flex-shrink-0 text-gray-300 fas fa-shield-alt fa-fw fa-5x"></i>
             </div>
@@ -57,7 +57,7 @@ Staff Hub
         </div>
     </a>
     <a @can('manage-recruitments') href="{{ route('staff.recruitment-management') }}" @endcan class="col-span-full md:col-span-2 @cannot('manage-recruitments') opacity-30 @endcannot">
-        <div class="p-8 text-gray-300 bg-gray-900 hover:bg-gray rounded-md text-center font-light tracking-wide">
+        <div class="w-full h-full p-8 text-gray-300 bg-gray-900 hover:bg-gray rounded-md text-center font-light tracking-wide">
             <div class="mb-5">
                 <i class="flex-shrink-0 text-gray-300 fas fa-briefcase fa-fw fa-5x"></i>
             </div>
@@ -65,7 +65,7 @@ Staff Hub
         </div>
     </a>
     <a @can('see-staff-members-list') href="{{ route('staff.staff-members-management') }}" @endcan class="col-span-full md:col-span-2 @cannot('see-staff-members-list') opacity-30 @endcannot">
-        <div class="p-8 text-gray-300 bg-gray-900 hover:bg-gray rounded-md text-center font-light tracking-wide">
+        <div class="w-full h-full p-8 text-gray-300 bg-gray-900 hover:bg-gray rounded-md text-center font-light tracking-wide">
             <div class="mb-5">
                 <i class="flex-shrink-0 text-gray-300 fas fa-users fa-fw fa-5x"></i>
             </div>
