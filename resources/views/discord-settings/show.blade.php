1b9363a2a5565e429b636cc89c003cf1e901a401
HerissonMan-TMP <013scrat@gmail.com>
2021-03-09 22:24:50 +0100

    Added basic content for website & discord settings page

---
+++ resources/views/discord-settings/show.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.staff')
+
+@section('title', 'Discord Settings')
+
+@section('breadcrumb', "Staff - Discord Settings")
+
+@section('content-staff')
+    <div class="px-4 py-5 md:p-6 bg-gray-800 rounded-md shadow overflow-hidden">
+        Discord settings
+    </div>
+@endsection
---
+++ resources/views/website-settings/show.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.staff')
+
+@section('title', 'Website Settings')
+
+@section('breadcrumb', "Staff - Website Settings")
+
+@section('content-staff')
+<div class="px-4 py-5 md:p-6 bg-gray-800 rounded-md shadow overflow-hidden">
+    Website settings
+</div>
+@endsection
