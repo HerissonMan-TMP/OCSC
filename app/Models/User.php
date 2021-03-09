0a2d5cb00a5e3396d604b320d61b79a899a5050d
HerissonMan-TMP <013scrat@gmail.com>
2021-03-09 18:30:50 +0100

    Fixed a mistake when checking if the user has a temporary password

--- app/Models/User.php
+++ app/Models/User.php
@@ -55,7 +55,7 @@ class User extends Authenticatable
      */
     public function getHasTemporaryPasswordAttribute()
     {
-        return $this->temporary_password_without_hash === null;
+        return $this->temporary_password_without_hash !== null;
     }
 
     /**
