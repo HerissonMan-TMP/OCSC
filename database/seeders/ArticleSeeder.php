cac3c1b19a430e57fd0cabdf9629003fa1cac38f
HerissonMan-TMP <013scrat@gmail.com>
2021-03-29 19:00:02 +0200

    Banner of article now depends of the URL stored in DB

--- database/seeders/ArticleSeeder.php
+++ database/seeders/ArticleSeeder.php
@@ -17,7 +17,7 @@ class ArticleSeeder extends Seeder
         $article = Article::create([
             'title' => 'Answer our community survey',
             'content' => '1.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-            'banner_url' => null,
+            'banner_url' => 'https://i.imgur.com/kZ3YjwR.png',
             'created_at' => now()->subDays(2),
             'updated_at' => now()->subDays(2),
         ]);
@@ -27,7 +27,7 @@ class ArticleSeeder extends Seeder
         $article = Article::create([
             'title' => 'MEGA Convoy - 28/05/2021 19:00 UTC',
             'content' => '2.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-            'banner_url' => null,
+            'banner_url' => 'https://i.imgur.com/kZ3YjwR.png',
             'created_at' => now()->subDays(10),
             'updated_at' => now()->subDays(10),
         ]);
@@ -37,7 +37,7 @@ class ArticleSeeder extends Seeder
         $article = Article::create([
             'title' => 'Update of our Convoy Rules',
             'content' => '3.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-            'banner_url' => null,
+            'banner_url' => 'https://i.imgur.com/kZ3YjwR.png',
             'created_at' => now()->subDays(15),
             'updated_at' => now()->subDays(15),
         ]);
@@ -47,7 +47,7 @@ class ArticleSeeder extends Seeder
         $article = Article::create([
             'title' => 'New Partnership | Welcome to SimNews!',
             'content' => '4.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-            'banner_url' => null,
+            'banner_url' => 'https://i.imgur.com/kZ3YjwR.png',
             'created_at' => now()->subDays(20),
             'updated_at' => now()->subDays(20),
         ]);
