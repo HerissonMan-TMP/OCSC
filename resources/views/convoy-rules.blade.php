25e90ddaa824d8ad10a4380ae05d464e11f08661
HerissonMan-TMP <013scrat@gmail.com>
2021-04-02 20:36:26 +0200

    Convoy rules page revamped

--- resources/views/convoy-rules.blade.php
+++ resources/views/convoy-rules.blade.php
@@ -2,18 +2,14 @@
 
 @section('title', 'Convoy Rules')
 
-@section('breadcrumb', 'Convoy Rules')
-
 @section('content')
-    <div class="max-w-7xl px-4 py-5 md:p-6 mx-auto my-16">
-        <div class="px-4 py-5 md:p-6 bg-gray-800 rounded-md shadow overflow-hidden">
-            <div class="flex justify-center items-center mt-2 mb-10">
-                <i class="inline-block fas fa-list-alt fa-fw fa-2x mr-2"></i>
-                <h2 class="m-0 inline-block font-bold text-gray-300">Convoy Rules</h2>
-            </div>
-            <div>
-                @markdown(setting('convoy-rules'))
-            </div>
+    <div class="flex flex-col justify-center items-center bg-fixed bg-cover bg-center py-52" style="background-image: linear-gradient(rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.6)), url('https://i.imgur.com/kZ3YjwR.png');">
+        <div class="text-center grid gap-4">
+            <h1 class="text-5xl m-0 capitalize"><i class="flex-shrink-0 fas fa-list-alt fa-fw"></i> Convoy Rules</h1>
         </div>
+    </div>
+
+    <div class="max-w-7xl px-4 py-5 md:p-6 mx-auto my-16">
+        @markdown(setting('convoy-rules'))
     </div>
 @endsection
