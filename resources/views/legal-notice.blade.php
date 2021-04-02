071a55060743f1fd927f859e652742a8e92238fe
HerissonMan-TMP <013scrat@gmail.com>
2021-04-02 20:40:49 +0200

    Revamped Legal Notice and Privacy Policy pages

--- resources/views/legal-notice.blade.php
+++ resources/views/legal-notice.blade.php
@@ -5,15 +5,13 @@
 @section('breadcrumb', 'Legal Notice')
 
 @section('content')
-    <div class="max-w-7xl px-4 py-5 md:p-6 mx-auto my-16">
-        <div class="px-4 py-5 md:p-6 bg-gray-800 rounded-md shadow overflow-hidden">
-            <div class="flex justify-center items-center mt-2 mb-10">
-                <i class="inline-block fas fa-balance-scale fa-fw fa-2x mr-2"></i>
-                <h2 class="m-0 inline-block font-bold text-gray-300">Legal Notice</h2>
-            </div>
-            <div>
-                @markdown(setting('legal-notice'))
-            </div>
+    <div class="flex flex-col justify-center items-center bg-fixed bg-cover bg-center py-52" style="background-image: linear-gradient(rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.6)), url('https://i.imgur.com/kZ3YjwR.png');">
+        <div class="text-center grid gap-4">
+            <h1 class="text-5xl m-0 capitalize"><i class="flex-shrink-0 fas fa-balance-scale fa-fw"></i> Legal Notice</h1>
         </div>
+    </div>
+
+    <div class="max-w-7xl px-4 py-5 md:p-6 mx-auto my-16">
+        @markdown(setting('legal-notice'))
     </div>
 @endsection
--- resources/views/privacy-policy.blade.php
+++ resources/views/privacy-policy.blade.php
@@ -5,15 +5,13 @@
 @section('breadcrumb', 'Privacy Policy')
 
 @section('content')
-    <div class="max-w-7xl px-4 py-5 md:p-6 mx-auto my-16">
-        <div class="px-4 py-5 md:p-6 bg-gray-800 rounded-md shadow overflow-hidden">
-            <div class="flex justify-center items-center mt-2 mb-10">
-                <i class="inline-block fas fa-user-secret fa-fw fa-2x mr-2"></i>
-                <h2 class="m-0 inline-block font-bold text-gray-300">Privacy Policy</h2>
-            </div>
-            <div>
-                @markdown(setting('privacy-policy'))
-            </div>
+    <div class="flex flex-col justify-center items-center bg-fixed bg-cover bg-center py-52" style="background-image: linear-gradient(rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.6)), url('https://i.imgur.com/kZ3YjwR.png');">
+        <div class="text-center grid gap-4">
+            <h1 class="text-5xl m-0 capitalize"><i class="flex-shrink-0 fas fa-user-secret fa-fw"></i> Privacy Policy</h1>
         </div>
+    </div>
+
+    <div class="max-w-7xl px-4 py-5 md:p-6 mx-auto my-16">
+        @markdown(setting('privacy-policy'))
     </div>
 @endsection
