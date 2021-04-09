232e8398a2b28def3eb8ac811648300b7f3db419
HerissonMan-TMP <013scrat@gmail.com>
2021-04-09 20:14:36 +0200

    Improved layout of the confirmation page after sending a contact message

--- resources/views/contact-messages/success-page.blade.php
+++ resources/views/contact-messages/success-page.blade.php
@@ -5,6 +5,12 @@
 @section('breadcrumb', 'Message sent')
 
 @section('content')
+<div class="flex flex-col justify-center items-center bg-fixed bg-cover bg-center py-52" style="background-image: linear-gradient(rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.6)), url('https://i.imgur.com/kZ3YjwR.png');">
+    <div class="text-center grid gap-4">
+        <h1 class="text-5xl m-0 capitalize"><i class="flex-shrink-0 fas fa-envelope fa-fw"></i> Contact</h1>
+    </div>
+</div>
+
 <div class="max-w-7xl mx-auto px-4 md:px-6 my-16">
     <div class="mt-6 bg-gray-800 rounded-md px-4 py-5 bg-gray-800 md:p-6 shadow overflow-hidden text-center">
         <h3 class="font-bold text-2xl text-gray-300 mt-2 mb-6">Your message has been successfully sent!</h3>
