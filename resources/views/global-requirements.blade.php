c22a99e5576279935de09ba788d7daa4765dda7c
HerissonMan-TMP <013scrat@gmail.com>
2021-04-09 19:09:10 +0200

    Changed layout of requirements page to match the other pages' UI

--- resources/views/global-requirements.blade.php
+++ resources/views/global-requirements.blade.php
@@ -5,19 +5,17 @@
 @section('breadcrumb', 'Global Requirements')
 
 @section('content')
-    <div class="max-w-7xl px-4 py-5 md:p-6 mx-auto my-16">
-        <div class="px-4 py-5 md:p-6 bg-gray-800 rounded-md shadow overflow-hidden">
-            <div class="flex justify-center items-center mt-2 mb-10">
-                <i class="inline-block fas fa-tasks fa-fw fa-2x mr-2"></i>
-                <h2 class="m-0 inline-block font-bold text-gray-300">Global Requirements for Recruitment</h2>
-            </div>
-            <div>
-                @if(setting('global-requirements'))
-                    @markdown(setting('global-requirements'))
-                @else
-                    <span class="italic">No global requirements set yet...</span>
-                @endif
-            </div>
+    <div class="flex flex-col justify-center items-center bg-fixed bg-cover bg-center py-52" style="background-image: linear-gradient(rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.6)), url('https://i.imgur.com/kZ3YjwR.png');">
+        <div class="text-center grid gap-4">
+            <h1 class="text-5xl m-0 capitalize"><i class="flex-shrink-0 fas fa-tasks fa-fw"></i> Global Requirements for Recruitment</h1>
         </div>
+    </div>
+
+    <div class="max-w-7xl px-4 py-5 md:p-6 mx-auto my-16">
+        @if(setting('global-requirements'))
+            @markdown(setting('global-requirements'))
+        @else
+            <span class="italic">No global requirements set yet...</span>
+        @endif
     </div>
 @endsection
