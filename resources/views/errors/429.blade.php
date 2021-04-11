c73f14d6bd64379dc4c9f32f71d0601b5336ebbb
HerissonMan-TMP <013scrat@gmail.com>
2021-04-11 13:17:24 +0200

    Configuring a rate limiter to avoid too many requests

---
+++ resources/views/errors/429.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.app')
+
+@section('title', "429 Too Many Requests")
+
+@section('breadcrumb', '429 Too Many Requests')
+
+@section('content')
+    <div class="max-w-7xl mx-auto px-4 md:px-6 my-16">
+        <div class="text-center mt-6 rounded-md px-4 py-5 md:p-16 overflow-hidden">
+            <div class="mb-8">
+                <span class="font-bold text-6xl text-gray-300 mt-2 mb-6">429</span>
+            </div>
+            <span class="font-light text-2xl text-gray-300 mt-2 mb-6">You sent too many requests.</span>
+        </div>
+    </div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -31,133 +31,135 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-//Public: Homepage.
-Route::get('/', [HomeController::class, 'homepage'])->name('homepage');
+Route::middleware(['throttle:web'])->group(function () {
+    //Public: Homepage.
+    Route::get('/', [HomeController::class, 'homepage'])->name('homepage');
 
-//Public: Articles.
-Route::resource('articles', ArticleController::class)->only([
-    'index', 'show'
-]);
-
-//Public: Convoy & Convoy rules.
-Route::get('upcoming-convoys', [ConvoyController::class, 'showUpcoming'])->name('convoys.show-upcoming');
-Route::view('convoy-rules', 'convoy-rules')->name('convoy-rules');
-
-//Public: Pictures (Gallery).
-Route::get('gallery', [PictureController::class, 'gallery'])->name('gallery');
-
-//Public: Recruitments & Global requirements.
-Route::get('recruitments/{recruitment}', [RecruitmentController::class, 'show'])->name('recruitments.show');
-Route::post('recruitments/{recruitment}/applications', [ApplicationController::class, 'store'])
-    ->name('recruitments.applications.store');
-Route::view('applications/success', 'applications.success-page')->name('applications.success-page');
-Route::view('global-requirements', 'global-requirements')->name('global-requirements');
-
-//Public: Contact messages.
-Route::view('contact/success', 'contact-messages.success-page')->name('contact-messages.success-page');
-Route::get('contact', [ContactMessageController::class, 'create'])->name('contact-messages.create');
-Route::post('contact', [ContactMessageController::class, 'store'])->name('contact-messages.store');
-
-//Public: Legal notice & Privacy policy.
-Route::view('legal-notice', 'legal-notice')->name('legal-notice');
-Route::view('privacy-policy', 'privacy-policy')->name('privacy-policy');
-
-//Public (without Staff): Login.
-Route::middleware(['guest'])->group(function () {
-    Route::view('login', 'login')->name('login.show-form');
-    Route::post('login', [LoginController::class, 'authenticate'])->name('login.authenticate');
-});
-
-//Staff: Temporary password.
-Route::middleware(['auth', 'temporary-password'])->group(function() {
-    Route::view('staff/temporary-password/edit', 'edit-temporary-password')
-        ->name('staff.temporary-password.edit');
-    Route::post('staff/temporary-password/update', [UserController::class, 'updateTemporaryPassword'])
-        ->name('staff.temporary-password.update');
-});
-
-//Staff without a password.
-Route::middleware(['auth', 'not-temporary-password'])->prefix('staff')->name('staff.')->group(function () {
-    //Hub.
-    Route::view('/', 'hub')->name('hub');
-
-    //Articles.
-    Route::get('articles/manage', [ArticleController::class, 'manage'])->name('articles.manage');
+    //Public: Articles.
     Route::resource('articles', ArticleController::class)->only([
-        'create', 'store', 'edit', 'update', 'destroy'
-    ]);
-
-    //Partnership.
-    Route::view('partnership-management', '')->name('partnership-management');
-
-    //Convoys & Convoy rules.
-    Route::resource('convoys', ConvoyController::class)->except([
-        'show'
-    ]);
-    Route::patch('convoy-rules', [ConvoyRulesController::class, 'update'])->name('convoy-rules.update');
-
-    //Pictures (Gallery).
-    Route::delete('pictures/delete-many', [PictureController::class, 'destroyMany'])
-        ->name('pictures.destroy-many');
-    Route::resource('pictures', PictureController::class)->except([
-        'show'
-    ]);
-
-    //Roles & Permissions.
-    Route::get('roles', [RoleController::class, 'index'])->name('roles.index');
-    Route::patch('roles/{role}/update-colors', [RoleController::class, 'updateColors'])
-        ->name('roles.update-colors');
-    Route::patch('roles/{role}/permissions', [PermissionController::class, 'update'])
-        ->name('roles.permissions.update');
-
-    //Recruitments (+ Questions & Applications) & Requirements.
-    Route::resource('recruitments', RecruitmentController::class)->except(
-        'show'
-    );
-    Route::resource('recruitments.questions', QuestionController::class)->only(
-        'store', 'update', 'destroy'
-    )->shallow();
-    Route::post('applications/{application}/accept', [ApplicationController::class, 'accept'])
-        ->name('applications.accept');
-    Route::post('applications/{application}/decline', [ApplicationController::class, 'decline'])
-        ->name('applications.decline');
-    Route::resource('recruitments.applications', ApplicationController::class)->only(
         'index', 'show'
-    )->shallow();
-    Route::patch('global-requirements', [GlobalRequirementsController::class, 'update'])
-        ->name('global-requirements.update');
-
-    //Contact messages.
-    Route::post('contact-messages/{contactMessage}/mark-as-read', [ContactMessageController::class, 'markAsRead'])
-        ->name('contact-messages.mark-as-read');
-    Route::post('contact-messages/{contactMessage}/mark-as-unread', [ContactMessageController::class, 'markAsUnread'])
-        ->name('contact-messages.mark-as-unread');
-    Route::resource('contact-messages', ContactMessageController::class)->only([
-        'index', 'show', 'destroy'
     ]);
 
-    //Users.
-    Route::patch('users/{user}/roles', [UserController::class, 'updateRoles'])->name('users.roles.update');
-    Route::resource('users', UserController::class)->except(
-        'edit', 'update'
-    );
+    //Public: Convoy & Convoy rules.
+    Route::get('upcoming-convoys', [ConvoyController::class, 'showUpcoming'])->name('convoys.show-upcoming');
+    Route::view('convoy-rules', 'convoy-rules')->name('convoy-rules');
 
-    //Downloads.
-    Route::resource('downloads', DownloadController::class)->except(
-        'show', 'create', 'edit'
-    );
+    //Public: Pictures (Gallery).
+    Route::get('gallery', [PictureController::class, 'gallery'])->name('gallery');
 
-    //Website Settings (+ Legal notice and Privacy policy).
-    Route::view('website-settings', 'website-settings.show')->name('website-settings');
-    Route::patch('legal-notice', [LegalNoticeController::class, 'update'])->name('legal-notice.update');
-    Route::patch('privacy-policy', [PrivacyPolicyController::class, 'update'])->name('privacy-policy.update');
+    //Public: Recruitments & Global requirements.
+    Route::get('recruitments/{recruitment}', [RecruitmentController::class, 'show'])->name('recruitments.show');
+    Route::post('recruitments/{recruitment}/applications', [ApplicationController::class, 'store'])
+        ->name('recruitments.applications.store');
+    Route::view('applications/success', 'applications.success-page')->name('applications.success-page');
+    Route::view('global-requirements', 'global-requirements')->name('global-requirements');
 
-    //Maintenance mode.
-    Route::post('maintenance-mode/enable', [MaintenanceModeController::class, 'enable'])
-        ->name('maintenance-mode.enable');
-    Route::post('maintenance-mode/disable', [MaintenanceModeController::class, 'disable'])
-        ->name('maintenance-mode.disable');
+    //Public: Contact messages.
+    Route::view('contact/success', 'contact-messages.success-page')->name('contact-messages.success-page');
+    Route::get('contact', [ContactMessageController::class, 'create'])->name('contact-messages.create');
+    Route::post('contact', [ContactMessageController::class, 'store'])->name('contact-messages.store');
 
-    //Logout.
-    Route::post('logout', [LoginController::class, 'logout'])->name('logout');
+    //Public: Legal notice & Privacy policy.
+    Route::view('legal-notice', 'legal-notice')->name('legal-notice');
+    Route::view('privacy-policy', 'privacy-policy')->name('privacy-policy');
+
+    //Public (without Staff): Login.
+    Route::middleware(['guest'])->group(function () {
+        Route::view('login', 'login')->name('login.show-form');
+        Route::post('login', [LoginController::class, 'authenticate'])->name('login.authenticate');
+    });
+
+    //Staff: Temporary password.
+    Route::middleware(['auth', 'temporary-password'])->group(function() {
+        Route::view('staff/temporary-password/edit', 'edit-temporary-password')
+            ->name('staff.temporary-password.edit');
+        Route::post('staff/temporary-password/update', [UserController::class, 'updateTemporaryPassword'])
+            ->name('staff.temporary-password.update');
+    });
+
+    //Staff without a password.
+    Route::middleware(['auth', 'not-temporary-password'])->prefix('staff')->name('staff.')->group(function () {
+        //Hub.
+        Route::view('/', 'hub')->name('hub');
+
+        //Articles.
+        Route::get('articles/manage', [ArticleController::class, 'manage'])->name('articles.manage');
+        Route::resource('articles', ArticleController::class)->only([
+            'create', 'store', 'edit', 'update', 'destroy'
+        ]);
+
+        //Partnership.
+        Route::view('partnership-management', '')->name('partnership-management');
+
+        //Convoys & Convoy rules.
+        Route::resource('convoys', ConvoyController::class)->except([
+            'show'
+        ]);
+        Route::patch('convoy-rules', [ConvoyRulesController::class, 'update'])->name('convoy-rules.update');
+
+        //Pictures (Gallery).
+        Route::delete('pictures/delete-many', [PictureController::class, 'destroyMany'])
+            ->name('pictures.destroy-many');
+        Route::resource('pictures', PictureController::class)->except([
+            'show'
+        ]);
+
+        //Roles & Permissions.
+        Route::get('roles', [RoleController::class, 'index'])->name('roles.index');
+        Route::patch('roles/{role}/update-colors', [RoleController::class, 'updateColors'])
+            ->name('roles.update-colors');
+        Route::patch('roles/{role}/permissions', [PermissionController::class, 'update'])
+            ->name('roles.permissions.update');
+
+        //Recruitments (+ Questions & Applications) & Requirements.
+        Route::resource('recruitments', RecruitmentController::class)->except(
+            'show'
+        );
+        Route::resource('recruitments.questions', QuestionController::class)->only(
+            'store', 'update', 'destroy'
+        )->shallow();
+        Route::post('applications/{application}/accept', [ApplicationController::class, 'accept'])
+            ->name('applications.accept');
+        Route::post('applications/{application}/decline', [ApplicationController::class, 'decline'])
+            ->name('applications.decline');
+        Route::resource('recruitments.applications', ApplicationController::class)->only(
+            'index', 'show'
+        )->shallow();
+        Route::patch('global-requirements', [GlobalRequirementsController::class, 'update'])
+            ->name('global-requirements.update');
+
+        //Contact messages.
+        Route::post('contact-messages/{contactMessage}/mark-as-read', [ContactMessageController::class, 'markAsRead'])
+            ->name('contact-messages.mark-as-read');
+        Route::post('contact-messages/{contactMessage}/mark-as-unread', [ContactMessageController::class, 'markAsUnread'])
+            ->name('contact-messages.mark-as-unread');
+        Route::resource('contact-messages', ContactMessageController::class)->only([
+            'index', 'show', 'destroy'
+        ]);
+
+        //Users.
+        Route::patch('users/{user}/roles', [UserController::class, 'updateRoles'])->name('users.roles.update');
+        Route::resource('users', UserController::class)->except(
+            'edit', 'update'
+        );
+
+        //Downloads.
+        Route::resource('downloads', DownloadController::class)->except(
+            'show', 'create', 'edit'
+        );
+
+        //Website Settings (+ Legal notice and Privacy policy).
+        Route::view('website-settings', 'website-settings.show')->name('website-settings');
+        Route::patch('legal-notice', [LegalNoticeController::class, 'update'])->name('legal-notice.update');
+        Route::patch('privacy-policy', [PrivacyPolicyController::class, 'update'])->name('privacy-policy.update');
+
+        //Maintenance mode.
+        Route::post('maintenance-mode/enable', [MaintenanceModeController::class, 'enable'])
+            ->name('maintenance-mode.enable');
+        Route::post('maintenance-mode/disable', [MaintenanceModeController::class, 'disable'])
+            ->name('maintenance-mode.disable');
+
+        //Logout.
+        Route::post('logout', [LoginController::class, 'logout'])->name('logout');
+    });
 });
