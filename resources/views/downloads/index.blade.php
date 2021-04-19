ebd77024eb9ebcf1d6baed1eda8a18de4f777130
HerissonMan-TMP <013scrat@gmail.com>
2021-04-19 19:58:03 +0200

    Improved the interface of downloads index

--- resources/views/downloads/index.blade.php
+++ resources/views/downloads/index.blade.php
@@ -18,36 +18,61 @@
             </div>
         @endif
 
-        <div>
-            <ul class="mt-10 mb-20">
+        <div class="shadow overflow-x-auto rounded-lg">
+            <table class="min-w-full divide-y divide-gray-800 border-none">
+                <thead class="bg-gray-700">
+                <tr>
+                    <th scope="col" class="border-none px-6 py-3 text-left text-xs font-medium text-gray-400 uppercase tracking-wider">
+                        ID
+                    </th>
+                    <th scope="col" class="border-none px-6 py-3 text-left text-xs font-medium text-gray-400 uppercase tracking-wider">
+                        Name
+                    </th>
+                    <th scope="col" class="border-none px-6 py-3 text-left text-xs font-medium text-gray-400 uppercase tracking-wider">
+                        Download
+                    </th>
+                    <th scope="col" class="border-none relative px-6 py-3">
+                        <span class="sr-only">Edit or delete</span>
+                    </th>
+                </tr>
+                </thead>
+                <tbody class="bg-gray-800">
                 @forelse($downloads as $download)
-                    <li>
-                        <span class="font-bold">{{ $download->name }}:</span>
-                        <a href="{{ $download->link }}" target="_blank">
-                            Download
-                            <i class="flex-shrink-0 fas fa-download fa-fw"></i>
-                        </a>
-                        @can('manage-downloads')
-                            <a href="{{ route('staff.downloads.edit', $download) }}" class="transition duration-200 text-primary hover:text-primary-dark focus:outline-none">
-                                Edit
-                                <i class="flex-shrink-0 fas fa-pen fa-fw"></i>
+                    <tr>
+                        <td class="border-none px-6 py-4 whitespace-nowrap text-sm">
+                            {{ $download->id }}
+                        </td>
+                        <td class="border-none px-6 py-4 whitespace-nowrap text-sm">
+                            {{ $download->name }}
+                        </td>
+                        <td class="border-none px-6 py-4 whitespace-nowrap text-sm">
+                            <a href="{{ $download->link }}" class="transition duration-200 text-primary hover:text-primary-dark focus:outline-none">
+                                Click to download
                             </a>
-
+                        </td>
+                        <td class="border-none px-6 py-4 whitespace-nowrap text-sm text-right">
+                            <a href="{{ route('staff.downloads.edit', $download) }}" class="mr-4 transition duration-200 text-primary hover:text-primary-dark focus:outline-none">
+                                <i class="flex-shrink-0 fas fa-pen fa-fw"></i>
+                                Edit
+                            </a>
                             <form action="{{ route('staff.downloads.destroy', $download) }}" method="POST" class="inline">
                                 @csrf
                                 @method('DELETE')
 
-                                <button type="submit" class="transition duration-200 text-red-500 hover:text-red-700 focus:outline-none">
-                                    Delete
+                                <button type="submit" class="transition duration-200 font-bold text-red-500 hover:text-red-600 focus:outline-none">
                                     <i class="flex-shrink-0 fas fa-trash-alt fa-fw"></i>
+                                    Delete
                                 </button>
                             </form>
-                        @endcan
-                    </li>
+                        </td>
+                    </tr>
                 @empty
-                    <span class="text-sm italic text-gray-300">No downloads available for you yet...</span>
+                    <tr>
+                        <td colspan="4" class="border-none px-6 py-4 whitespace-nowrap text-sm italic text-gray-300">No available downloads yet...</td>
+                    </tr>
                 @endforelse
-            </ul>
+                </tbody>
+            </table>
         </div>
     </div>
 @endsection
