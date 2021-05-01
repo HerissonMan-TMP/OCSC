ba7c1ef75d8ac7a4c7e38a2c50c728af49046bfc
HerissonMan-TMP <013scrat@gmail.com>
2021-05-01 15:46:18 +0200

    Adding a changelog box on the Staff Hub page

--- resources/views/hub.blade.php
+++ resources/views/hub.blade.php
@@ -10,7 +10,7 @@
 
         <div class="grid grid-cols-8 gap-6">
             <div class="col-span-full md:col-span-3">
-                <div class="h-full p-4 rounded-md bg-gray-800">
+                <div class="p-4 rounded-md bg-gray-800">
                     <h3 class="mt-0 mb-8">Basic Statistics</h3>
 
                     <div class="self-center">
@@ -42,9 +42,44 @@
 
             <div class="col-span-full md:col-span-5">
                 <div class="p-4 rounded-md bg-gray-800">
-                    <h3 class="mt-0 mb-8">Other...</h3>
+                    <div class="mb-8">
+                        <h3 class="mt-0 mb-2">Website Changelog</h3>
+                        <span class="text-sm text-gray-400">Latest changelog: <span class="font-bold">01/05/2021 13:00</span></span>
+                    </div>
 
+                    <div class="grid grid-cols-2 gap-6">
+                        <div class="col-span-full md:col-span-1 flex items-center gap-4">
+                            <div class="w-10 h-10 p-4 bg-yellow-500 rounded-md flex items-center justify-center">
+                                <i class="fas fa-star fa-fw"></i>
+                            </div>
 
+                            <span class="text-sm">Convoys data is now directly fetched from to the TruckersMP API.</span>
+                        </div>
+
+                        <div class="col-span-full md:col-span-1 flex items-center gap-4">
+                            <div class="w-10 h-10 p-4 bg-red-500 rounded-md flex items-center justify-center">
+                                <i class="fas fa-bug fa-fw"></i>
+                            </div>
+
+                            <span class="text-sm">Fixing incorrect default values in some inputs when attempting to edit a user's name & email.</span>
+                        </div>
+
+                        <div class="col-span-full md:col-span-1 flex items-center gap-4">
+                            <div class="w-10 h-10 p-4 bg-green-500 rounded-md flex items-center justify-center">
+                                <i class="fas fa-thumbs-up fa-fw"></i>
+                            </div>
+
+                            <span class="text-sm">Adding a lot of feature tests (used for testing if the website features are working correctly).</span>
+                        </div>
+
+                        <div class="col-span-full md:col-span-1 flex items-center gap-4">
+                            <div class="w-10 h-10 p-4 bg-green-500 rounded-md flex items-center justify-center">
+                                <i class="fas fa-thumbs-up fa-fw"></i>
+                            </div>
+
+                            <span class="text-sm">Cleaning code.</span>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
