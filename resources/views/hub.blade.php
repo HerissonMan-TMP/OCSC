8c829995d290f5fbcce75ab352d4c4d3b64275fe
HerissonMan-TMP <013scrat@gmail.com>
2021-06-01 09:58:33 +0200

    Updating the changelog message on the staff hub

--- resources/views/hub.blade.php
+++ resources/views/hub.blade.php
@@ -46,7 +46,7 @@
                         <div class="h-full p-4 rounded-md bg-gray-800">
                             <div class="mb-8">
                                 <h3 class="mt-0 mb-2">Website Changelog</h3>
-                                <span class="text-sm text-gray-400">Latest changelog: <span class="font-bold">02/05/2021 09:15 UTC</span></span>
+                                <span class="text-sm text-gray-400">Latest changelog: <span class="font-bold">01/06/2021 07:55 UTC</span></span>
                             </div>
 
                             <div class="grid grid-cols-2 gap-6">
@@ -55,7 +55,7 @@
                                         <i class="fas fa-thumbs-up fa-fw"></i>
                                     </div>
 
-                                    <span class="text-sm">Revamping the Staff Hub homepage.</span>
+                                    <span class="text-sm">Changing the text displayed when a partnership category is open.</span>
                                 </div>
 
                                 <div class="col-span-full md:col-span-1 flex items-center gap-4">
@@ -63,7 +63,7 @@
                                         <i class="fas fa-thumbs-up fa-fw"></i>
                                     </div>
 
-                                    <span class="text-sm">Increasing the duration before the convoys cache is refreshed (from 5 to 10 minutes).</span>
+                                    <span class="text-sm">Displaying only upcoming convoys on the public Convoys page.</span>
                                 </div>
 
                                 <div class="col-span-full md:col-span-1 flex items-center gap-4">
@@ -71,7 +71,7 @@
                                         <i class="fas fa-thumbs-up fa-fw"></i>
                                     </div>
 
-                                    <span class="text-sm">Removing some too severe rate limiting (numbers of requests / second limitation).</span>
+                                    <span class="text-sm">Fixing displaying the global requirements on every recruitment page.</span>
                                 </div>
                             </div>
                         </div>
