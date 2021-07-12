e6885715529cf126877d8153824037cf1744e69a
HerissonMan-TMP <013scrat@gmail.com>
2021-07-12 19:09:44 +0200

    Adding Discord notifications when toggling maintenance mode

--- app/Http/Controllers/MaintenanceModeController.php
+++ app/Http/Controllers/MaintenanceModeController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\MaintenanceMode\EnableMaintenanceModeRequest;
 use App\Models\ActivityType;
+use App\Services\DiscordEmbed;
 use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Gate;
 
@@ -29,6 +30,21 @@ class MaintenanceModeController extends Controller
             '--secret' => $request->secret
         ]);
 
+        (new DiscordEmbed())
+            ->webhook(config('discord_webhooks.general'))
+            ->username('OCSC Event - Worker')
+            ->author('OCSC Event', config('app.url'), asset('img/ocsc_logo.png'))
+            ->color(1096065)
+            ->thumbnail(config('app.url') . '/img/ocsc_logo.png')
+            ->title('🛠 - Maintenance Mode')
+            ->description('The website\'s maintenance mode has just been **enabled**.
+                You will be notified when it will become available again.
+                Thank you for your patience!'
+            )
+            ->image('https://media.discordapp.net/attachments/824978783051448340/849887295611994152/ets2_20210515_230820_00.png?width=1246&height=701')
+            ->footer('https://ocsc.fr', asset('img/ocsc_logo.png'))
+            ->send();
+
         activity(ActivityType::ENABLED)
             ->subject('fas fa-wrench', 'Maintenance Mode')
             ->log();
@@ -49,6 +65,18 @@ class MaintenanceModeController extends Controller
         Gate::authorize('toggle-maintenance-mode');
 
         Artisan::call('up');
+
+        (new DiscordEmbed())
+            ->webhook(config('discord_webhooks.general'))
+            ->username('OCSC Event - Worker')
+            ->author('OCSC Event', config('app.url'), asset('img/ocsc_logo.png'))
+            ->color(15680580)
+            ->thumbnail(config('app.url') . '/img/ocsc_logo.png')
+            ->title('🛠 - Maintenance Mode')
+            ->description('The website\'s maintenance mode has just been **disabled**. You can browse our website again!')
+            ->image('https://media.discordapp.net/attachments/824978783051448340/849887295611994152/ets2_20210515_230820_00.png?width=1246&height=701')
+            ->footer('https://ocsc.fr', asset('img/ocsc_logo.png'))
+            ->send();
 
         activity(ActivityType::DISABLED)
             ->subject('fas fa-wrench', 'Maintenance Mode')
