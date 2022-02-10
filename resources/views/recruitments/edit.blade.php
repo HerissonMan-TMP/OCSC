0b344e59ceb2c4d41b06b676cc138e3dbc2f4b49
HerissonMan-TMP <013scrat@gmail.com>
2022-02-10 16:15:34 +0100

    Fixing bug in recruitments edition page if user is anonymous

--- resources/views/recruitments/edit.blade.php
+++ resources/views/recruitments/edit.blade.php
@@ -15,7 +15,7 @@
             <div class="grid grid-cols-6 gap-6">
                 <div class="col-span-full md:col-span-3">
                     <label class="block text-sm font-medium text-gray-300">Opened By</label>
-                    <input type="text" disabled style="color: {{ $recruitment->user->roles->first()->color }}" class="text-gray-300 bg-gray-800 font-bold mt-1 focus:ring-primary-dark focus:border-primary-dark block w-full shadow-sm md:text-sm border-gray-700 rounded-md" value="{{ $recruitment->user->name }} (you)">
+                    <input type="text" disabled style="color: {{ $recruitment->user?->roles->first()->color }}" class="text-gray-300 bg-gray-800 font-bold mt-1 focus:ring-primary-dark focus:border-primary-dark block w-full shadow-sm md:text-sm border-gray-700 rounded-md" value="{{ $recruitment->user?->name ?: 'Anonymous' }}">
                 </div>
 
                 <div class="col-span-full md:col-span-full">
